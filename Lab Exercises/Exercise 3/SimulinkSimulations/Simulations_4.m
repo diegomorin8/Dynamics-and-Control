8121afc8e207f0bbbfb1b30577b5509a53f68fda
diegogm <diegogm>
2017-03-03 11:55:48 +0100

    Fucking german pipol

--- Lab Exercises/Exercise 3/SimulinkSimulations/Simulations_4.m
+++ Lab Exercises/Exercise 3/SimulinkSimulations/Simulations_4.m
@@ -9,10 +9,13 @@ Psp = double(Psp_d);
 Isp = double(Isp_d);
 Dsp = double(Dsp_d);
 Nsp = double(Nsp_d);
+% Simulink
 
 %Step response with and without controller
 Sine_wave = 0; % 0 - Step input - 1 - Sine wave input
 time = 2;
+Amplitude = 20;
+Freq = 0.2;
 %Call the simulink model
 sim('SimLab3_4')
 
@@ -32,7 +35,7 @@ stairs(VoltageLTime, VoltageL);
 hold on 
 stairs(VoltageFrictionTime, VoltageFriction);
 legend('Voltage L','Voltage Friction');
-axis([0 time 0 20])
+% axis([0 time 0 20])
 title('Control signal (voltage) - Step')
 hold off
 
@@ -41,7 +44,7 @@ stairs(PositionLTime, PositionL);
 hold on 
 stairs(PositionFrictionTime, PositionFriction);
 legend('Position L','Position Friction');
-axis([0 time 0 20])
+% axis([0 time 0 20])
 title('Controlled state (Position) - Step')
 hold off
 
@@ -69,7 +72,7 @@ stairs(VoltageLTime, VoltageL);
 hold on 
 stairs(VoltageFrictionTime, VoltageFriction);
 legend('Voltage L','Voltage Friction');
-axis([0 time -20 20])
+% axis([0 time -20 20])
 title('Control signal (voltage) - Sin 0.2')
 hold off
 
@@ -77,15 +80,18 @@ subplot(3,2,4)
 stairs(PositionLTime, PositionL);
 hold on 
 stairs(PositionFrictionTime, PositionFriction);
-legend('Position L','Position Friction');
-axis([0 time -20 20])
+t = 0:0.01:time;
+u = Amplitude*sin(Freq*t);
+stairs(t,u);
+legend('Position L','Position Friction','Signal ref');
+% axis([0 time -20 20])
 title('Controlled state (Position) - Sin 0.2')
 hold off
 
 
 %Sin wave response
 Sine_wave = 1; % 0 - Step input - 1 - Sine wave input
-Amplitude = 200;
+Amplitude = 20;
 Freq = 10;
 time = 1;
 %Call the simulink model
@@ -106,7 +112,7 @@ stairs(VoltageLTime, VoltageL);
 hold on 
 stairs(VoltageFrictionTime, VoltageFriction);
 legend('Voltage L','Voltage Friction');
-axis([0 time -400 400])
+% axis([0 time -200 200])
 title('Control signal (voltage) - Sin 10')
 hold off
 
@@ -114,7 +120,10 @@ subplot(3,2,6)
 stairs(PositionLTime, PositionL);
 hold on 
 stairs(PositionFrictionTime, PositionFriction);
-legend('Position L','Position Friction');
-axis([0 time -200 200])
+t = 0:0.01:time;
+u = Amplitude*sin(Freq*t);
+stairs(t,u);
+legend('Position L','Position Friction','Signal ref');
+% axis([0 time -200 200])
 title('Controlled state (Position) - Sin 10')
 hold off
